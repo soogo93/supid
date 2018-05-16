e1e4b917babe79f1e315f0f52d992562b62c4078
seungsoo Ha <330soo@naver.com>
2018-05-16 16:47:08 +0900

    init

---
+++ src/main/java/com/su/supid/SupidApplication.java
@@ -0,0 +1,11 @@
+package com.su.supid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupidApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SupidApplication.class, args);
+	}
+}
